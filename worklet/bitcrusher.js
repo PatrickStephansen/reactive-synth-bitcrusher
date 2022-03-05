f8c34a696de4defe218c8f0b470bef8c37533029
Patrick Stephansen <pstephansen89@gmail.com>
2022-03-05 13:00:43 +0200

    compile wasmModule for every instance
    This is to work around a chrome messageport issue.
    It no longer serializes and deserializes webassembly modules.

--- worklet/bitcrusher.js
+++ worklet/bitcrusher.js
@@ -44,6 +44,7 @@ registerProcessor(
 		}
 
 		async initWasmModule(wasmModule) {
+			wasmModule = await WebAssembly.compile(wasmModule);
 			this.wasmModule = await WebAssembly.instantiate(wasmModule, {});
 			this.internalProcessorPtr = this.wasmModule.exports.init(
 				128,
